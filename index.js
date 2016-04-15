c028fbd85ce3fd3539d673009fa5312d63761dee
Andrew Szucs <andrew0szucs@gmail.com>
2016-04-16 00:38:16 +0200

    Added Generic Message sending capability and Postback handling.

--- index.js
+++ index.js
@@ -17,6 +17,111 @@ const FB_PAGE_ID = process.env.FB_PAGE_ID && Number(process.env.FB_PAGE_ID);
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
 
+// Messaging functions
+//Function to Send Text Messages
+function sendTextMessage(sender, text) {
+    messageData = {
+        text: text
+    };
+    
+    request(
+        {
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: { access_token: FB_PAGE_TOKEN },
+            method: 'POST',
+            json: {
+                recipient: { id: sender },
+                message: messageData,
+            }
+        }, 
+        function(error, response, body) {
+            if(error) {
+                console.log('Error sending messages: ', error);
+            }
+            else if(response.body.error) {
+                console.log('Error: ', response.body.error);
+            }
+        }
+    );
+}
+
+// Function to Send Structured Messages (Generic Message template)
+function sendGenericMessage(sender) {
+    messageData = {
+        
+        "attachment": {
+            "type": "template",
+            
+            "payload": {
+                "template_type": "generic",
+                
+                //Array of cards in a horizontal scroll
+                "elements": [ 
+                    // One card
+                    {
+                        "title": "First card",
+                        "subtitle": "Element #1 of an hscroll.",
+                        "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                        // Buttons on Card ONE
+                        "buttons": [
+                            // First Button: takes to a URL
+                            {
+                                "type": "web_url",
+                                "url": "https://www.messenger.com",
+                                "title": "Web url"
+                            },
+                            // Second Button: makes a postback backend call to
+                            // the webhook when the button is tapped
+                            {
+                                "type": "postback",
+                                "title": "Postback",
+                                "payload": "Payload for first element in a generic bubble"
+                            }
+                        ]
+                    },
+                    
+                    // Second card
+                    {
+                        "title": "Second card",
+                        "subtitle": "Element #2 of an hscroll",
+                        "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                        "buttons": [
+                            // First Button: postback call
+                            {
+                                "type": "postback",
+                                "title": "Postback",
+                                "payload": "Payload for second element in generic bubble"
+                            }
+                        ]
+                    }
+                ],
+            },
+        }  
+    };
+    
+    request(
+        {
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs:{ access_token: FB_PAGE_TOKEN },
+            method: 'POST',
+            json: {
+                recipient: { id: sender },
+                message: messageData,
+            }
+        }, 
+        function(error, response, body) {
+            if(error) {
+                console.log('Error sending messages ', error);
+            }
+            else if(response.body.error) {
+                console.log('Error: ', response.body.error);
+            }
+        }
+    );
+    
+}
+
+
 // Express settings
 const app = express();
 app.set('port', PORT);
@@ -50,33 +155,6 @@ app.get('/webhook', function(req, res) {
     }
 });
 
-//Function to ECHO back messages
-function sendTextMessage(sender, text) {
-    messageData = {
-        text: text
-    };
-    
-    request(
-        {
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: FB_PAGE_TOKEN },
-            method: 'POST',
-            json: {
-                recipient: { id: sender },
-                message: messageData,
-            }
-        }, 
-        function(error, response, body) {
-            if(error) {
-                console.log('Error sending messages: ', error);
-            }
-            else if(response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        }
-    );
-}
-
 // API endpoint To process messages
 app.post('/webhook', function(req, res) {
     messaging_events = req.body.entry[0].messaging;
@@ -85,7 +163,21 @@ app.post('/webhook', function(req, res) {
         sender = event.sender.id;
         if(event.message && event.message.text) {
             text = event.message.text;
+            
+            // if text IS 'Generic' we can send a Generic Message
+            if(text === 'Generic') {
+                sendGenericMessage(sender);
+                // 'continue': Start the next iteration of the loop immediately
+                continue;
+            }
+            // if text is NOT 'Generic' we can send a TEXT Message
             sendTextMessage(sender, "Text received, echo: " + text.substring(0,200));
+        }
+        // if user clicks on postback button
+        if(event.postback) {
+            text = JSON.stringify(event.postback);
+            sendTextMessage(sender, "Postback received: " + text.substring(0, 200), FB_PAGE_TOKEN);
+            continue;
         }
     }
     res.sendStatus(200);
